2a6a3a3feebe76e57b08a8e8da1df49521bcc4b6
AkCoder <netakdeniz@gmail.com>
2025-08-30 20:55:58 +0300

    verid

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,19 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import com.android.build.gradle.BaseExtension
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        // JitPack repository'sini kaldırıyoruz
+        // Jitpack repo
+        maven("https://jitpack.io")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
-        // CloudStream gradle plugin'ini kaldırıyoruz
+        classpath("com.android.tools.build:gradle:8.7.3")
+        // Cloudstream gradle plugin (commit hash ile sabitledik)
+        classpath("com.github.recloudstream:gradle:cce1b8d84d")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -16,11 +21,83 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        // Sadece gerekli repository'leri tutuyoruz
-        maven { url "https://maven.lagradost.cloud/releases" }
+        maven("https://jitpack.io")
+    }
+
+    // JADB master-SNAPSHOT yerine v1.2.1 sürümünü zorla
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "com.github.vidstige" && requested.name == "jadb") {
+                useVersion("v1.2.1")
+                because("master-SNAPSHOT jitpack'te yok, v1.2.1 kullanılmalı")
+            }
+        }
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/keyiflerolsun/Kekik-cloudstream")
+        authors = listOf("keyiflerolsun")
+    }
+
+    android {
+        namespace = "com.keyiflerolsun"
+
+        defaultConfig {
+            minSdk = 21
+            compileSdkVersion(35)
+            targetSdk = 35
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+                freeCompilerArgs.addAll(
+                    listOf(
+                        "-Xno-call-assertions",
+                        "-Xno-param-assertions",
+                        "-Xno-receiver-assertions"
+                    )
+                )
+            }
+        }
+    }
+
+    dependencies {
+        val cloudstream by configurations
+        val implementation by configurations
+
+        // Cloudstream stub
+        cloudstream("com.lagradost:cloudstream3:4.5.5")
+
+        // Bağımlılıklar
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.13")
+        implementation("org.jsoup:jsoup:1.19.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+        implementation("com.github.vidstige:jadb:v1.2.1") // doğru sürüm
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
