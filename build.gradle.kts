92460e9a838300381c644bce18b790174fd8fd73
AkCoder <netakdeniz@gmail.com>
2025-08-30 18:25:18 +0300

    change

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        cloudstream("com.lagradost:cloudstream3:1.0.0")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
